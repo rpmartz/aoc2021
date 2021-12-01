2c91307fac99b70841b7f286978df5feeb8d7972
rpmartz <ryan@ryanpmartz.com>
2021-12-01 07:44:50 -0800

    reformat

--- day_01.py
+++ day_01.py
@@ -1,13 +1,9 @@
-
-
 def read_file():
     with open('data/day01.txt', 'r') as f:
         return [int(line.strip()) for line in f.readlines()]
 
 
-
 def do_part_one():
-
     lines = read_file()
     num_increases = 0
 
