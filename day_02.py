32cb3b2915500b08a804c208b4548e5f6b48f3c7
rmartz <martzrp@gmail.com>
2021-12-01 22:56:41 -0800

    part two

--- day_02.py
+++ day_02.py
@@ -21,6 +21,29 @@ def part_one(lines):
     print('End position is %s, %s' % (x, y))
     print('Product of end position is %d' % (x * y))
 
+def part_two(lines):
+    print('\n\n=== part two ===\n\n')
+    horizontal_pos = 0
+    depth = 0
+    aim = 0
+
+    for line in lines:
+        components = line.split(' ')
+        direction = components[0]
+        magnitude = int(components[1])
+
+        if direction == 'forward':
+            horizontal_pos = horizontal_pos + magnitude
+            depth = depth + (aim * magnitude)
+        elif direction == 'up':
+            aim = aim - magnitude
+        elif direction == 'down':
+            aim = aim + magnitude
+
+    print('End position is horizontal_pos: %s, depth: %s aim: %s' % (horizontal_pos, depth, aim))
+    print('Product of end position is %d' % (horizontal_pos * depth))
+
 if __name__ == '__main__':
     lines = read_file()
     part_one(lines)
+    part_two(lines)
