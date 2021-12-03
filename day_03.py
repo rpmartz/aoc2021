0f3d6198e71734c97f0d92faa8bb0d5f54e6cbc1
rmartz <martzrp@gmail.com>
2021-12-02 22:26:27 -0800

    part two

--- day_03.py
+++ day_03.py
@@ -43,9 +43,94 @@ def part_one(lines):
     print('epsilon: %d' % (epsilon))
     print('gamma * epsilon = %d' % (gamma * epsilon))
 
+def take_where(binary_strings, position, value):
+    """
+    Filters a collection of binary strings whose value at index `position` are equal to `value`
+    """
+
+    # defensive checks
+    assert position < 12
+    assert value in {'0', '1'}
+
+    result = []
+    for binary_string in binary_strings:
+        if binary_string[position] == value:
+            result.append(binary_string)
+
+    return result
+
+def part_two(lines):
+    # life_support_rating = oxygen_generator_rating * co2_scrubbter_rating
+    positions_to_bit_counts = {
+        0: {'0': 0, '1': 0},
+        1: {'0': 0, '1': 0},
+        2: {'0': 0, '1': 0},
+        3: {'0': 0, '1': 0},
+        4: {'0': 0, '1': 0},
+        5: {'0': 0, '1': 0},
+        6: {'0': 0, '1': 0},
+        7: {'0': 0, '1': 0},
+        8: {'0': 0, '1': 0},
+        9: {'0': 0, '1': 0},
+        10: {'0': 0, '1': 0},
+        11: {'0': 0, '1': 0}
+    }
+
+    lines = read_file()
+    for line in lines:
+        for index, bit in enumerate(line):
+            bit_count_for_index = positions_to_bit_counts[index]
+
+            count_for_bit_at_index = bit_count_for_index[bit]
+            count_for_bit_at_index += 1
+            bit_count_for_index[bit] = count_for_bit_at_index
+
+
+    o2_gen_rating = None
+    o2_results = lines
+    for i in range(0, 12):
+        bit_counts = positions_to_bit_counts[i]
+        if bit_counts['0'] > bit_counts['1']:
+            more_freq_bit = '0'
+        else:
+            print('keeping numbers with bit of 1 at position %d' % i)
+            more_freq_bit = '1'
+
+        o2_results = take_where(o2_results, i, more_freq_bit)
+        if len(o2_results) == 1:
+            o2_gen_rating = o2_results[0]
+
+
+    if o2_gen_rating is None:
+        raise Exception('Could not find O2 rating')
+
+    co2_scrubber_rating = None
+    co2_results = lines
+    for i in range(0, 12):
+        bit_counts = positions_to_bit_counts[i]
+        if bit_counts['0'] < bit_counts['1']:
+            less_frequent_bit = '0'
+        else:
+            less_frequent_bit = '1'
+
+        co2_results = take_where(co2_results, i, less_frequent_bit)
+        if len(co2_results) == 1:
+            co2_scrubber_rating = co2_results[0]
+
+    if co2_scrubber_rating is None:
+        raise Exception('Could not find CO2 scrubber rating')
+
+
+    co2_scrubber_as_int = int(''.join(co2_scrubber_rating), 2)
+    o2_gen_rating_as_int = int(''.join(o2_gen_rating), 2)
+
+    print(co2_scrubber_as_int * o2_gen_rating_as_int)
+
+
 
 if __name__ == '__main__':
     lines = read_file()
     part_one(lines)
 
     print('\n=== part two ===\n')
+    part_two(lines)
