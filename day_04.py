cbebd00c96a7b9aa3d232e08d22a43ceb363f9be
rmartz <martzrp@gmail.com>
2021-12-03 23:22:21 -0800

    implement bingo checks

--- day_04.py
+++ day_04.py
@@ -1,7 +1,7 @@
-
 def read_file():
     with open('data/day04.txt', 'r') as f:
         return [line.strip() for line in f.readlines()]
+
 
 def build_boards(lines):
     boards = []
@@ -17,6 +17,7 @@ def build_boards(lines):
 
     return boards
 
+
 def mark_number_seen(number, boards):
     for board in boards:
         for row in board:
@@ -25,9 +26,40 @@ def mark_number_seen(number, boards):
                     row[index] = (number, True)
 
 
+def has_row_bingo(board):
+    for row in board:
+        if row[0][1] and row[1][1] and row[2][1] and row[3][1] and row[4][1]:
+            return True
+
+    return False
+
+
+def has_column_bingo(board):
+    for column in range(0, 5):
+        if board[0][column][1] and board[1][column][1] and board[2][column][1] and board[3][column][1] and \
+                board[4][column][1]:
+            return True
+
+    return False
+
+
+def has_diagonal_bingo(board):
+    return board[0][0][1] and board[1][1][1] and board[2][2][1] and board[3][3][1] and board[4][4][1]
+
+
+def has_bingo(board):
+    return has_row_bingo(board) or has_column_bingo(board) or has_diagonal_bingo(board)
+
 
 lines = read_file()
 bingo_input = lines[0]
 
 boards = build_boards(lines)
 
+for number in bingo_input:
+    mark_number_seen(int(number), boards)
+
+    for board in boards:
+        if has_bingo(board):
+            print('Bingo for board on number %s' % number)
+            print(board)
