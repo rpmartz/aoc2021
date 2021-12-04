b9e0f9b6157d0fe190720dd54accc9aadaf0146f
rmartz <martzrp@gmail.com>
2021-12-03 22:52:53 -0800

    refactor board into function

--- day_04.py
+++ day_04.py
@@ -3,18 +3,24 @@ def read_file():
     with open('data/day04.txt', 'r') as f:
         return [line.strip() for line in f.readlines()]
 
+def build_boards(lines):
+    boards = []
+    current_board = []
+    for line in lines[2:]:
+        if not line:
+            boards.append(current_board)
+            current_board = []
+
+        current_board.append([(int(num), False) for num in line.split(' ') if num != ''])
+
+    return boards
+
 
 lines = read_file()
 bingo_input = lines[0]
 
-boards = []
-current_board = []
-for line in lines[2:]:
-    if not line:
-        boards.append(current_board)
-        current_board = []
+boards = build_boards(lines)
 
-    current_board.append([(int(num), False) for num in line.split(' ') if num != ''])
 
 
 
