4210cdf33681b703d4001c534ec8c79411befdd6
rmartz <martzrp@gmail.com>
2021-12-04 22:05:19 -0800

    build board

--- day_05.py
+++ day_05.py
@@ -2,6 +2,21 @@ def read_file():
     with open('data/day05.txt', 'r') as f:
         return [line.strip() for line in f.readlines()]
 
+def build_board():
+    """
+    build 1000 x 1000 board of zeros
+    """
+    board = list()
+    for i in range(0, 1000):
+        row = []
+        for j in range(0, 1000):
+            row.append(0)
+
+        board.append(row)
+
+    return board
+
 
 if __name__ == '__main__':
     lines = read_file()
+    board = build_board()
