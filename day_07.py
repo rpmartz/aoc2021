8645f830b1d3f9c301d0d15b020863c6b5a62b8b
rpmartz <ryan@ryanpmartz.com>
2021-12-07 04:31:50 -0800

    PEP-8

--- day_07.py
+++ day_07.py
@@ -2,6 +2,7 @@ def read_file():
     with open('data/day07.txt', 'r') as f:
         return [int(num) for num in f.read().split(',')]
 
+
 def total_cost(num_steps):
     cost = 0
     cost_for_step = 1
@@ -10,6 +11,7 @@ def total_cost(num_steps):
         cost_for_step += 1
 
     return cost
+
 
 def find_best_alignment(positions, cost_fn):
     min_distance = 999999999999999
@@ -22,6 +24,7 @@ def find_best_alignment(positions, cost_fn):
             best_position = number
 
     return best_position, min_distance
+
 
 if __name__ == '__main__':
     starting_positions = read_file()
