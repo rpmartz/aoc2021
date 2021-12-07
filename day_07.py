8431b10f176d9cf069362a15f0b88486e3d8f425
rmartz <martzrp@gmail.com>
2021-12-06 22:26:06 -0800

    return total cost and position

--- day_07.py
+++ day_07.py
@@ -1,16 +1,18 @@
-import math
 
 
 def find_best_alignment(positions):
     min_distance = 999999999999999
+    best_position = None
     for number in positions:
         distance_for_number = 0
         for point in positions:
             distance_for_number += abs(number - point)
 
-        min_distance = min(min_distance, distance_for_number)
+        if distance_for_number < min_distance:
+            min_distance = distance_for_number
+            best_position = number
 
-    return min_distance
+    return best_position, min_distance
 
 
 
--- tests/test_day07.py
+++ tests/test_day07.py
@@ -7,6 +7,6 @@ class DaySeven(unittest.TestCase):
     def test_example_case(self):
         input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
 
-        expected = 10
+        expected = 2,37
         actual = find_best_alignment(input)
         self.assertEqual(expected, actual)
