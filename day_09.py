3a11098f84875bcba9b3e5bddaca2c4be36147a7
rpmartz <ryan@ryanpmartz.com>
2021-12-09 15:32:13 -0800

    add test for finding neighbors

--- day_09.py
+++ day_09.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def read_board():
     with open('data/day09.txt', 'r') as f:
         board = []
@@ -30,6 +33,32 @@ def calculate_risk_level(low_points):
     return sum(1 + p[2] for p in low_points)
 
 
+def get_coordinates_of_neighbors(x, y, board):
+    neighbors = []
+
+    # left neighbor
+    if x > 0:
+        neighbors.append((x - 1, y))
+
+    # right
+    if x + 1 < len(board[0]) - 1:
+        neighbors.append((x + 1, y))
+
+    # top
+    if y > 0:
+        neighbors.append((x, y - 1))
+    # bottom
+    if y + 1 < len(board) - 1:
+        neighbors.append((x, y + 1))
+
+    return neighbors
+
+
+def find_basin_sizes(low_points, board) -> List[int]:
+    for point in low_points:
+        x, y = point[0], point[1]
+
+
 def do_part_one():
     board = read_board()
     low_points = find_low_points(board)
@@ -40,7 +69,10 @@ def do_part_one():
 
 def do_part_two():
     board = read_board()
+    low_points = find_low_points(board)
+    basins = find_basin_sizes(low_points, board)
 
 
 if __name__ == '__main__':
     do_part_one()
+    do_part_two()
