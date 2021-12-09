0ff325ee99a77c2dcc29aa55aa72c070d248f0a2
rpmartz <ryan@ryanpmartz.com>
2021-12-09 15:14:00 -0800

    test for finding basin

--- tests/test_day09.py
+++ tests/test_day09.py
@@ -21,3 +21,18 @@ class DayNineTest(unittest.TestCase):
     def test_summing_risk_score(self):
         low_points = [(0, 1, 1), (0, 9, 0), (2, 2, 5), (4, 6, 5)]
         self.assertEqual(15, calculate_risk_level(low_points))
+
+    def test_finding_basins(self):
+        board = [
+            [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+            [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+            [9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+            [8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+            [9, 8, 9, 9, 9, 6, 5, 6, 7, 8]
+        ]
+
+        low_points = find_low_points(board)
+        expected = [3, 9, 14, 9]
+        actual = find_basin_sizes(low_points)
+
+        self.assertEqual(expected, actual)
