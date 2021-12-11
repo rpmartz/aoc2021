05bb7fa2e86552407f6caed462f4dd0b812037db
rmartz <martzrp@gmail.com>
2021-12-11 06:55:08 -0800

    day 11 start

---
+++ day_11.py
@@ -0,0 +1,3 @@
+
+def count_num_flashes(board, iteration):
+    pass
---
+++ tests/test_day11.py
@@ -0,0 +1,24 @@
+from day_11 import *
+import unittest
+
+
+class DayEleven(unittest.TestCase):
+
+    def test_example(self):
+        board = [
+            [5, 4, 8, 3, 1, 4, 3, 2, 2, 3],
+            [2, 7, 4, 5, 8, 5, 4, 7, 1, 1],
+            [5, 2, 6, 4, 5, 5, 6, 1, 7, 3],
+            [6, 1, 4, 1, 3, 3, 6, 1, 4, 6],
+            [6, 3, 5, 7, 3, 8, 5, 4, 7, 8],
+            [4, 1, 6, 7, 5, 2, 4, 6, 4, 5],
+            [2, 1, 7, 6, 8, 4, 1, 7, 2, 1],
+            [6, 8, 8, 2, 8, 8, 1, 1, 3, 4],
+            [4, 8, 4, 6, 8, 4, 8, 5, 5, 4],
+            [5, 2, 8, 3, 7, 5, 1, 5, 2, 6]
+        ]
+
+        expected = 1656
+        actual = count_num_flashes(board, 100)
+
+        self.assertEqual(expected, actual)
