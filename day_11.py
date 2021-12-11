5f19ecc72d9b443f8484adbcfe3ab35438b6d2ea
rpmartz <ryan@ryanpmartz.com>
2021-12-11 10:47:11 -0800

    neighbors implemented

--- day_11.py
+++ day_11.py
@@ -1,10 +1,62 @@
 def count_num_flashes(board, iteration):
     for step in range(iteration):
+
+        flashed_this_step = set()
+
         for i, row in enumerate(board):
             for j, element in enumerate(row):
                 if element < 9:
                     board[i][j] = board[i][j] + 1
                 # if element has flashed, increment neighbors by one
                 elif element == 9:
-                    # do flash
+                    process_flash(board, i, j, flashed_this_step)
                     board[i][j] = 0
+
+
+def process_flash(board, i, j, already_flashed):
+    neighbors = get_neighbors(board, i, j)
+    for neighbor in neighbors:
+        x, y = neighbor
+        if board[x][y] < 9 and neighbor not in already_flashed:
+            board[x][y] = board[x][y] + 1
+        elif board[x][y] == 9 and neighbor not in already_flashed:
+            already_flashed.add(neighbor)
+            board[x][y] = 0
+            process_flash(board, x, y, already_flashed)
+        else:
+            raise Exception('Should not reach this point during recursion')
+
+
+def get_neighbors(board, i, j):
+    height = len(board)
+    width = len(board[0])
+
+    neighbors = set()
+    # left
+    if j > 0:
+        neighbors.add((i, j - 1))
+    # right
+    if j + 1 < width:
+        neighbors.add((i, j + 1))
+    # top
+    if i > 0:
+        neighbors.add((i - 1, j))
+    # bottom
+    if i + 1 < height:
+        neighbors.add((i + 1, j))
+    # diagonal upper left
+    if i > 0 and j > 0:
+        neighbors.add((i - 1, j - 1))
+
+    # diagonal upper right
+    if i > 0 and j + 1 < width:
+        neighbors.add((i - 1, j + 1))
+
+    # diagonal lower left
+    if i + 1 < height and j > 0:
+        neighbors.add((i + 1, j - 1))
+    # diagonal lower right
+    if i + 1 < height and j + 1 < width:
+        neighbors.add((i + 1, j + 1))
+
+    return set(neighbors)
--- tests/test_day11.py
+++ tests/test_day11.py
@@ -2,22 +2,22 @@ import unittest
 
 from day_11 import *
 
+board = [
+    [5, 4, 8, 3, 1, 4, 3, 2, 2, 3],
+    [2, 7, 4, 5, 8, 5, 4, 7, 1, 1],
+    [5, 2, 6, 4, 5, 5, 6, 1, 7, 3],
+    [6, 1, 4, 1, 3, 3, 6, 1, 4, 6],
+    [6, 3, 5, 7, 3, 8, 5, 4, 7, 8],
+    [4, 1, 6, 7, 5, 2, 4, 6, 4, 5],
+    [2, 1, 7, 6, 8, 4, 1, 7, 2, 1],
+    [6, 8, 8, 2, 8, 8, 1, 1, 3, 4],
+    [4, 8, 4, 6, 8, 4, 8, 5, 5, 4],
+    [5, 2, 8, 3, 7, 5, 1, 5, 2, 6]
+]
 
 class DayEleven(unittest.TestCase):
 
     def test_example(self):
-        board = [
-            [5, 4, 8, 3, 1, 4, 3, 2, 2, 3],
-            [2, 7, 4, 5, 8, 5, 4, 7, 1, 1],
-            [5, 2, 6, 4, 5, 5, 6, 1, 7, 3],
-            [6, 1, 4, 1, 3, 3, 6, 1, 4, 6],
-            [6, 3, 5, 7, 3, 8, 5, 4, 7, 8],
-            [4, 1, 6, 7, 5, 2, 4, 6, 4, 5],
-            [2, 1, 7, 6, 8, 4, 1, 7, 2, 1],
-            [6, 8, 8, 2, 8, 8, 1, 1, 3, 4],
-            [4, 8, 4, 6, 8, 4, 8, 5, 5, 4],
-            [5, 2, 8, 3, 7, 5, 1, 5, 2, 6]
-        ]
 
         expected = 1656
         actual = count_num_flashes(board, 100)
@@ -25,7 +25,7 @@ class DayEleven(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_sub_example(self):
-        board = [
+        subsample_board = [
             [1, 1, 1, 1, 1],
             [1, 9, 9, 9, 1],
             [1, 9, 1, 9, 1],
@@ -33,6 +33,55 @@ class DayEleven(unittest.TestCase):
             [1, 1, 1, 1, 1]
         ]
 
-        count_num_flashes(board, 1)
-        for row in board:
+        count_num_flashes(subsample_board, 1)
+        for row in subsample_board:
             print(row)
+
+    def test_get_neighbors_upper_left(self):
+        neighbors = get_neighbors(board, 0, 0)
+        self.assertEqual(len(neighbors), 3)
+
+        expected = {(0, 1), (1, 0), (1, 1)}
+        self.assertEqual(expected, neighbors)
+
+    def test_get_neighbors_upper_right(self):
+        neighbors = get_neighbors(board, 0, 9)
+        self.assertEqual(len(neighbors), 3)
+
+        expected = {(0, 8), (1, 8), (1, 9)}
+        self.assertEqual(expected, neighbors)
+
+    def test_get_neighbors_lower_left(self):
+        neighbors = get_neighbors(board, 9, 0)
+        self.assertEqual(len(neighbors), 3)
+
+        expected = {(9, 1), (8, 0), (8, 1)}
+        self.assertEqual(expected, neighbors)
+
+    def test_get_neighbors_lower_right(self):
+        neighbors = get_neighbors(board, 9, 9)
+        self.assertEqual(len(neighbors), 3)
+
+        expected = {(9, 8), (8, 8), (8, 9)}
+        self.assertEqual(expected, neighbors)
+
+    def test_get_neighbors_middle(self):
+        neighbors = get_neighbors(board, 2, 3)
+        self.assertEqual(len(neighbors), 8)
+
+        expected = {(1, 2), (1, 3), (1, 4), (2, 2), (2, 4), (3, 2), (3, 3), (3, 4)}
+        self.assertEqual(expected, neighbors)
+
+    def test_get_neighbors_top_middle(self):
+        neighbors = get_neighbors(board, 0, 3)
+        self.assertEqual(len(neighbors), 5)
+
+        expected = {(0, 2), (0, 4), (1, 2), (1, 3), (1, 4)}
+        self.assertEqual(expected, neighbors)
+
+    def test_get_neighbors_bottom_middle(self):
+        neighbors = get_neighbors(board, 9, 3)
+        self.assertEqual(len(neighbors), 5)
+
+        expected = {(9, 2), (9, 4), (8, 2), (8, 3), (8, 4)}
+        self.assertEqual(expected, neighbors)
