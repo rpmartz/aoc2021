f51735232a7709601aec360e28f63fae8048e1f0
rpmartz <ryan@ryanpmartz.com>
2021-12-13 18:22:08 -0800

    read points

--- day_13.py
+++ day_13.py
@@ -3,7 +3,13 @@ def read_points():
         return [(int(line.strip().split(',')[0]), int(line.strip().split(',')[1])) for line in f.readlines()]
 
 
+def read_folds():
+    with open('data/day13-folds.txt', 'r') as f:
+        return [(line.split('=')[0], int(line.split('=')[1])) for line in f.readlines()]
+
+
 if __name__ == '__main__':
     lines = read_points()
-    for line in lines:
-        print(lines)
+    folds = read_folds()
+    for fold in folds:
+        print(fold)
