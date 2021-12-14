78cfbdfa466558a2ef3898aece473914de963d61
rpmartz <ryan@ryanpmartz.com>
2021-12-13 18:43:50 -0800

    implement day 13

--- day_13.py
+++ day_13.py
@@ -8,8 +8,40 @@ def read_folds():
         return [(line.split('=')[0], int(line.split('=')[1])) for line in f.readlines()]
 
 
+def process_fold(points, fold):
+    points_after_fold = set()
+
+    axis, value = fold
+    for idx, point in enumerate(points):
+        p_x, p_y = point
+        if axis == 'x':
+            # fold right half left
+            if p_x > value:
+                # x value of point shifts twice distance from p_x to value
+                distance = value - p_x
+                new_point = (-2 * distance, p_y)
+                points_after_fold.add(new_point)
+            else:
+                points_after_fold.add(point)
+
+        elif axis == 'y':
+            # fold bottom half up
+            if p_y > value:
+                distance = value - p_x
+                new_point = (p_x, -2 * distance)
+                points_after_fold.add(new_point)
+            else:
+                points_after_fold.add(point)
+        else:
+            raise Exception('Do not know how to handle %s' % fold)
+
+    return points_after_fold
+
+
 if __name__ == '__main__':
-    lines = read_points()
+    points = read_points()
     folds = read_folds()
-    for fold in folds:
-        print(fold)
+
+    first_fold = folds[0]
+    points = process_fold(points, first_fold)
+    print(len(points))
