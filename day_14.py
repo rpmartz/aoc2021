dfe591be3dddf0f1c571b1e289541adaa2e89796
rpmartz <ryan@ryanpmartz.com>
2021-12-14 18:50:27 -0800

    tests for sample and implementation of polymer

--- day_14.py
+++ day_14.py
@@ -1,1 +1,23 @@
+from collections import Counter
+
 problem_start_template = 'OFSNKKHCBSNKBKFFCVNB'
+
+
+def read_rules():
+    with open('data/day14.txt', 'r') as f:
+        return f.read()
+
+
+def process(template, rules):
+    pass
+
+
+def quantify(polymer):
+    counter = Counter()
+    counter.update([c for c in polymer])
+
+    return max(counter.values()) - min(counter.values())
+
+
+if __name__ == '__main__':
+    pass
