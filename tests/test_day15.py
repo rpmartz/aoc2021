c3cf33d41b41e69abd1270d6791b20c15d2712ad
rpmartz <ryan@ryanpmartz.com>
2021-12-16 20:20:46 -0800

    test (failing) for example

---
+++ tests/test_day15.py
@@ -0,0 +1,26 @@
+import unittest
+
+from aocutils import read_numeric_grid
+from day_15 import calculate_min_risk
+
+example_input = """\
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+"""
+
+
+class DayFifteenTest(unittest.TestCase):
+
+    def test_problem_example_risk_score(self):
+        expected = 40
+        actual = calculate_min_risk(read_numeric_grid(example_input))
+
+        self.assertEqual(expected, actual)
