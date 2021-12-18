c9144eb1b41f4c06b30d2abf093e88cc9c717e2c
rpmartz <ryan@ryanpmartz.com>
2021-12-18 09:41:58 -0800

    loop that's not quite right

--- day_17.py
+++ day_17.py
@@ -26,3 +26,26 @@ if __name__ == '__main__':
     corners = {(150, -70), (150, -129), (171, -129), (171, -129)}
     if not all([point in target_grid for point in corners]):
         print('Check bounds')
+
+    initial_position = (0, 0)
+    max_y = -999999999
+    for d_x in range(172, 0, -1):
+        for d_y in range(-130, 500):
+
+            # d_x, d_y is vector of initial launch
+            # run steps
+            position = (0 + d_x, 0 + d_y)
+            # iterate until either probe in target or we know probe missed
+            heights = set()
+            while True:
+                heights.add(position[1])
+                position = process_step(position[0], position[1])
+
+                if position in target_grid:
+                    # capture max height and end iteration
+                    max_y = max(max_y, max(heights))
+                    break
+                elif position[0] > 171 or position[1] < -129:
+                    break
+
+    print(max_y)
