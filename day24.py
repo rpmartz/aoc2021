f0d6d9d19c40aee2b874d8b24efef57d621e4f41
rmartz <martzrp@gmail.com>
2021-12-24 13:45:05 -0800

    implement algorithm

--- day24.py
+++ day24.py
@@ -3,19 +3,11 @@ def eql(a, b):
         return 1
 
     return 0
-#
-#
-#
-#
-# start = int(14 * '9')
-#
-# for i in range(start, 0, -1):
-#     model_number = str(i)
-#     if '0' in model_number:
-#         continue
-#
-#     # todo run ALU
-#     pass
+
+
+
+
+
 
 def parse(symbol: str):
     if symbol.isalpha():
@@ -41,7 +33,7 @@ def parse_instructions():
         else:
             op, first, second = line.split()
             if op == 'inp':
-                stmt = f'{first} = model_number[index]'
+                stmt = f'{first} = int(model_number[index])'
                 parsed_instructions.append(stmt)
                 parsed_instructions.append('index += 1')
             elif op == 'mul':
@@ -62,7 +54,290 @@ def parse_instructions():
 
     return parsed_instructions
 
+def is_valid(model_number):
+    x, y, z = 0, 0, 0
+    index = 0
+
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 10
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 13
+    y = y * x
+    z = z + y
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 13
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 10
+    y = y * x
+    z = z + y
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 13
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 3
+    y = y * x
+    z = z + y
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + -11
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 1
+    y = y * x
+    z = z + y
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 11
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 9
+    y = y * x
+    z = z + y
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + -4
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 3
+    y = y * x
+    z = z + y
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 12
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 5
+    y = y * x
+    z = z + y
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 12
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 1
+    y = y * x
+    z = z + y
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 15
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 0
+    y = y * x
+    z = z + y
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + -2
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 13
+    y = y * x
+    z = z + y
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + -5
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 7
+    y = y * x
+    z = z + y
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + -11
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 15
+    y = y * x
+    z = z + y
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + -13
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 12
+    y = y * x
+    z = z + y
+    w = int(model_number[index])
+    index += 1
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + -10
+    x = eql(x, w)
+    x = eql(x, 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 8
+    y = y * x
+    z = z + y
+
+    return z == 0
+
 if __name__ == '__main__':
     inst = parse_instructions()
     with open('24.txt', 'w') as f:
         f.writelines('\n'.join(inst))
+
+    start = int(14 * '9')
+
+    for i in range(start, 0, -1):
+        model_number = str(i)
+        if '0' in model_number:
+            continue
+        elif is_valid(model_number):
+            print(model_number)
+            break
