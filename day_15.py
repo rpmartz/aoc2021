c9fd6108f174dc576ec095fc714c5db61faca5f1
rpmartz <ryan@ryanpmartz.com>
2021-12-27 19:24:41 -0800

    update to keep track of where we came from

--- day_15.py
+++ day_15.py
@@ -8,8 +8,11 @@ def read_file():
 
 def calculate_min_risk(grid) -> int:
     start = Point(0, 0)
-
     queue = [start]
+
+    came_from = dict()
+    came_from[start] = None
+
     while queue:
         position = queue.pop(0)
         neighbors = get_neighbors(position, 8)
@@ -19,13 +22,17 @@ def calculate_min_risk(grid) -> int:
                 # some neighbors will be off of the board/grid
                 continue
 
-            if grid[neighbor] != 'reached':
-                grid[neighbor] = 'reached'
+            # todo: modify for Djikstra's so that we can prioritize which path to explore
+            #
+            if neighbor not in came_from:
+                came_from[neighbor] = position
                 queue.append(neighbor)
+
+    print(came_from)
 
     return -2
 
 
 if __name__ == '__main__':
     grid = read_numeric_grid(read_file())
-    print(grid)
+    calculate_min_risk(grid)
