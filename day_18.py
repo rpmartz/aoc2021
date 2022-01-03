fc65344bfb27bab81da47c58c8d382caa6f2ad3d
rpmartz <ryan@ryanpmartz.com>
2022-01-02 16:37:46 -0800

    start of 18

--- day_18.py
+++ day_18.py
@@ -4,3 +4,15 @@ if __name__ == '__main__':
 
     for line in lines:
         print(line.strip())
+
+
+def calculate_magnitude(snailfish_number):
+    pass
+
+# need to implement addition for snailfish numbers
+
+# [1, 2] + [[3,4], 5] = [[1,2],[[3,4],5]]
+
+# to reduce a snailfish number, you must repeatedly do first action in list that applies:
+##  - if any pair nested inside 4 pairs, leftmost pair explodes
+##  - if any number is 10 or greater, leftmost regular number splits
